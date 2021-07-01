101a5fb3fe82db2daf994adfa227c0131e9e9e93
Wilson Cheah <wilsoncheah@gmail.com>
2021-06-30 19:21:42 -0700

    - added username and password field - created username and password state - created a handleChange function for the username and passworld fields

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const response = await fetch('/api/create_new_account', {
@@ -11,7 +14,15 @@ export default function CreateAccount() {
     });
 
     console.log(await response.json());
-  }
+  };
+
+  const handleChange = (event: any) => {
+    if (event.target.name === 'username') {
+      setUsername(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    }
+  };
 
   return (
     <>
@@ -19,7 +30,16 @@ export default function CreateAccount() {
         <title>Create Account</title>
       </Head>
       <article className={styles.article}>
+        <h1>Create New Account</h1>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <label>
+            Username
+            <input type='text' name='username' value={username} onChange={handleChange}></input>
+          </label>
+          <label>
+            Password
+            <input type='password' name='password' value={password} onChange={handleChange}></input>
+          </label>
           <button>Create Account</button>
         </form>
       </article>
