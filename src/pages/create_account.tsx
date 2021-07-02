30740af0c611c2b58a35f406176d3d775625f136
Wilson Cheah <wilsoncheah@gmail.com>
2021-07-02 16:46:36 -0700

    added handling for invalid usernames and passwords by displaying a tooltip with the requirements. This tooltip will only appear when the respective input is in focus. When the user leaves the input, it will hide the tooltip and only display a red * next to the label

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,14 +1,17 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { FormEvent, useState } from 'react';
-import ExposedBypass from '../components/ExposedBypass';
+import ExposedBypass from 'src/components/ExposedBypass';
+import ToolTip from 'src/components/ToolTip';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
-  const [ hasFormError, setHasFormError ] = useState(false);
-  const [ formError, setFormError ] = useState('');
+  const [ hasProccessingError, setHasProcessingError ] = useState(false);
+  const [ hasUsernameError, setHasUsernameError ] = useState(false);
+  const [ hasPasswordError, setHasPasswordError ] = useState(false);
+  const [ showUsernameToolTip, setShowUsernameToolTip ] = useState(false);
+  const [ showPasswordToolTip, setShowPasswordToolTip ] = useState(false);
   const [ exposed, setExposed ] = useState(false);
   const [ exposedWarning, setExposedWarning] = useState(false);
   const [ bypassWarning, setBypassWarning ] = useState(false);
@@ -30,6 +33,7 @@ export default function CreateAccount() {
       }
 
       if (!resExposed.result || (exposed && bypassWarning)) {
+        setExposedWarning(false);
         const rawResponseCreate = await fetch('/api/create_new_account', {
           method: 'POST',
           body: JSON.stringify({ username, password }),
@@ -39,26 +43,51 @@ export default function CreateAccount() {
         if (res.result) {
           console.log('account created!');
         } else {
-          setHasFormError(true);
-          setFormError('Error creating account. Please try again.');
+          if (res.errors.isUsernameValid) setHasUsernameError(true);
+          if (res.errors.isPasswordValid) setHasPasswordError(true);
         }
       }
     } catch (e) {
-      setHasFormError(true);
-      setFormError('Error processing request. Please try again.')
+      setHasProcessingError(true);
     }
   };
 
   const handleChange = (event: any) => {
     if (exposed) setExposed(false);
     if (bypassWarning) setBypassWarning(false);
-    if (hasFormError) {
-      setHasFormError(false);
-      setFormError('');
-    };
-    if (event.target.name === 'username') setUsername(event.target.value);
-    if (event.target.name === 'password') setPassword(event.target.value);
+    if (event.target.name === 'username') {
+      if (hasUsernameError) {
+        setHasUsernameError(false);
+      };
+      setUsername(event.target.value);
+    }
+    if (event.target.name === 'password') {
+      if (hasPasswordError) {
+        setHasPasswordError(false);
+      };
+      setPassword(event.target.value);
+    }
   };
+
+  const usernameError = hasUsernameError
+    ? <span style={{color: '#a20000'}}>
+        * {
+            showUsernameToolTip
+            ? <ToolTip minChar={10} maxChar={50} leftPos={'75px'}/>
+            : null
+          }
+      </span>
+    : null;
+
+  const passwordError = hasPasswordError
+    ? <span style={{color: '#a20000'}}>
+        * {
+            showPasswordToolTip
+            ? <ToolTip minChar={20} maxChar={50} leftPos={'70px'}/>
+            : null
+          }
+      </span>
+    : null;
 
   return (
     <>
@@ -68,16 +97,31 @@ export default function CreateAccount() {
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
           <img src="/logo.png" />
+          {/* <Image src={logo} alt='wealthfront logo' /> */}
           <h1>Create New Account</h1>
-          {hasFormError ? <p>{formError}</p> : null}
+          {hasProccessingError ? <p>Error processing request. Please try again.</p> : null}
           {exposedWarning ? <p>Warning! Our records show that your password may be been exposed. We recommend that you choose a different password.</p> : null}
-          <label>
-            Username
-            <input type='text' name='username' value={username} onChange={handleChange}></input>
+          <label style={{position: 'relative'}}>
+            Username {usernameError}
+            <input
+              type='text'
+              name='username'
+              value={username}
+              onChange={handleChange}
+              onFocus={() => setShowUsernameToolTip(true)}
+              onBlur={() => setShowUsernameToolTip(false)}
+              onKeyPress={(e) => {if (e.key === 'Enter') e.currentTarget.blur()}} ></input>
           </label>
-          <label>
-            Password
-            <input type='password' name='password' value={password} onChange={handleChange}></input>
+          <label style={{position: 'relative'}}>
+            Password {passwordError}
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={handleChange}
+              onFocus={() => setShowPasswordToolTip(true)}
+              onBlur={() => setShowPasswordToolTip(false)}
+              onKeyPress={(e) => {if (e.key === 'Enter') e.currentTarget.blur()}} ></input>
           </label>
           {
             exposed
