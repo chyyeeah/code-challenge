4958ce9f730094d4ecb8cc11fb9842957bf8e536
Wilson Cheah <wilsoncheah@gmail.com>
2021-07-01 23:08:24 -0700

    - added error messages if there are issues with the form submission - added a password exposed warning dependent on the API - added a way for the user to continue despite their password being exposed Logic of handling submit: 1. check if password has been exposed   - stop progressing with submission   - password exposure warning will appear 2. if password is not exposed OR (password is exposed and the user bypasses the warning) POST the username and password to API 3. check if account creation was successful   - if successful, currently console.log "account created"   - if unsuccessful, display form error

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,30 +1,61 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { FormEvent, useState } from 'react';
+import ExposedBypass from '../components/ExposedBypass';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
-  const [ submitError, setSubmitError ] = useState(false);
+  const [ hasFormError, setHasFormError ] = useState(false);
+  const [ formError, setFormError ] = useState('');
+  const [ exposed, setExposed ] = useState(false);
+  const [ exposedWarning, setExposedWarning] = useState(false);
+  const [ bypassWarning, setBypassWarning ] = useState(false);
 
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
-    const response = await fetch('/api/create_new_account', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-    });
-    const res = await response.json();
-    console.log(res);
+    setExposedWarning(false);
+    try {
+      const rawResponseExposed = await fetch('/api/password_exposed', {
+        method: 'POST',
+        body: JSON.stringify({ password })
+      });
 
-    if (res.result) {
-      console.log('ready to check if password is exposed');
-    } else {
-      setSubmitError(true);
+      const resExposed = await rawResponseExposed.json();
+      console.log('exposed', resExposed);
+      if (resExposed.result) {
+        setExposed(true);
+        setExposedWarning(true);
+      }
+
+      if (!resExposed.result || (exposed && bypassWarning)) {
+        const rawResponseCreate = await fetch('/api/create_new_account', {
+          method: 'POST',
+          body: JSON.stringify({ username, password }),
+        });
+        const res = await rawResponseCreate.json();
+        console.log('account creation', res);
+        if (res.result) {
+          console.log('account created!');
+        } else {
+          setHasFormError(true);
+          setFormError('Error creating account. Please try again.');
+        }
+      }
+    } catch (e) {
+      setHasFormError(true);
+      setFormError('Error processing request. Please try again.')
     }
   };
 
   const handleChange = (event: any) => {
-    if (submitError) setSubmitError(false);
+    if (exposed) setExposed(false);
+    if (bypassWarning) setBypassWarning(false);
+    if (hasFormError) {
+      setHasFormError(false);
+      setFormError('');
+    };
     if (event.target.name === 'username') setUsername(event.target.value);
     if (event.target.name === 'password') setPassword(event.target.value);
   };
@@ -36,8 +67,10 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <img src="/logo.png" />
           <h1>Create New Account</h1>
-          {submitError ? <p>Error!</p> : null}
+          {hasFormError ? <p>{formError}</p> : null}
+          {exposedWarning ? <p>Warning! Our records show that your password may be been exposed. We recommend that you choose a different password.</p> : null}
           <label>
             Username
             <input type='text' name='username' value={username} onChange={handleChange}></input>
@@ -46,7 +79,12 @@ export default function CreateAccount() {
             Password
             <input type='password' name='password' value={password} onChange={handleChange}></input>
           </label>
-          <button>Create Account</button>
+          {
+            exposed
+            ? <ExposedBypass
+                bypassWarning={bypassWarning}
+                setBypassWarning={setBypassWarning} /> : null}
+          <button name="create-account">Create Account</button>
         </form>
       </article>
     </>
