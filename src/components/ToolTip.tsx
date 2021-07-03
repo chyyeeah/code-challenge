b8d61cd08862b29b115d57b0d4714cffe4585551
Wilson Cheah <wilsoncheah@gmail.com>
2021-07-02 18:45:27 -0700

    refactored styles in ToolTip

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -1,7 +1,24 @@
 const ToolTip = ({ minChar, maxChar, leftPos }) => {
+  const toolTipStyles: Record<string, string> = {
+    position: 'absolute',
+    top: '-100px',
+    left: leftPos,
+    fontSize: '.90em',
+    backgroundColor: '#fff',
+    border: '1px solid lightgray',
+    borderRadius: '10px',
+    boxShadow: '0 2px 14px rgba(0, 0, 0, 0.15)'
+  };
+
+  const toolTipListStyles: Record<string, string> = {
+    margin: '0',
+    padding: '5px 10px 5px 25px',
+    letterSpacing: '0.5px'
+  };
+
   return (
-    <div style={{ position: 'absolute', top: '-100px', left: leftPos, fontSize: '.90em', backgroundColor: '#fff', border: '1px solid lightgray',borderRadius: '10px'}}>
-      <ul style={{margin: 0, padding: '5px 10px 5px 25px', letterSpacing: '0.5px'}}>
+    <div style={toolTipStyles}>
+      <ul style={toolTipListStyles}>
         <li>Must be at least {minChar} characters</li>
         <li>Must be {maxChar} characters or less</li>
         <li>Must contain at least 1 symbol [!@#$%]</li>
